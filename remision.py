64269e281aad31feb39455afb7ae8cdab002584d
Armando Rosales <refill.1509@gmail.com>
2024-10-08 13:04:54 -0600

    Correccion link remision

--- remision.py
+++ remision.py
@@ -5,4 +5,4 @@ from utils.texto import guion
 st.title('Busqueda de remisiones')
 num_remison = st.text_input('No. Remision')
 
-st.link_button('Consultar',f"http://10.236.21.166:8000/remision/ver/{guion(num_remison)}")
+st.link_button('Consultar',f"http://127.0.0.1:8000/remision/ver/{guion(num_remison)}")
