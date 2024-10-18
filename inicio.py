b5b683462026d207cffc1ea46224b4223db524a4
Armando Rosales <refill.1509@gmail.com>
2024-10-18 10:41:10 -0600

    actualizacion del puerto en inicio

--- inicio.py
+++ inicio.py
@@ -7,7 +7,7 @@ st.header('Página :orange[remisiones]')
 login.generarLogin()
 if 'usuario' in st.session_state:
     with st.form('frmLogin'):
-        hostname = '10.236.20.86:8501'
+        hostname = '10.236.20.86:8000'
         st.title('Busqueda de remisiones')
         num_remison = st.text_input('No. Remision')
         btnBuscar = st.form_submit_button('Buscar', type='primary')
