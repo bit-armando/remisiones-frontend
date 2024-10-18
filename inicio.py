93e91f80897cef748e54a94311dbcd6cecb4cc9b
Armando Rosales <refill.1509@gmail.com>
2024-10-18 10:40:05 -0600

    actualizacion del hostname en inicio

--- inicio.py
+++ inicio.py
@@ -7,7 +7,7 @@ st.header('Página :orange[remisiones]')
 login.generarLogin()
 if 'usuario' in st.session_state:
     with st.form('frmLogin'):
-        hostname = '127.0.0.1:8000'
+        hostname = '10.236.20.86:8501'
         st.title('Busqueda de remisiones')
         num_remison = st.text_input('No. Remision')
         btnBuscar = st.form_submit_button('Buscar', type='primary')
